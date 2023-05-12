b6dc030f865a4239fa6f884aefbe98bf3ff422fe
kiendepzai123 <133374462+kiendepzai123@users.noreply.github.com>
2023-05-12 20:45:37 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+  text1: "He luu Thảooo!",
+  text2: "Tuii có điều này muốn hỏi Thảoo nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Thảoo yêu tuii có phải không nào ._.",
+  text4: "Nếu Thảoo ko trả lời mà thoát ra tức là muốn làm vợ tuii rùi đó nha :v",
+  text5: "M mơ à???",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text7: "Tui biết mà >:> thử nói lí do thảo thích tuii đi :vvvv",
+  text8: "Gửi cho tuii <3",
+  text9: "Vì kinn đẹp try vlllll",
+  text10: "Tuii biết mà ^^ Yêu Thảoo 300.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Tối nay tuy không chơi được nhưng hẹn ngày gần nhất có thể nhaa :v Còn giờ thì chờ gì nữa mà ko inbox nói iu tuii đi nàooo :D",
   text12: "Okii lunn <3",
 };
 
